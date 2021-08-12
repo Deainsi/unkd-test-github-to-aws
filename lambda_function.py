d83d10d2919f187a39b30089ab303f0142ffa1aa
Deains <deinsid@gmail.com>
2021-08-12 23:28:34 +0300

    Test

--- lambda_function.py
+++ lambda_function.py
@@ -4,6 +4,6 @@ import json
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello AWS')
+        'body': json.dumps('Hello World')
     }
 
